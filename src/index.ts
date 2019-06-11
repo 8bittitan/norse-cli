f9dbe11045d4ecebc2e6a6c7dc4d89a8ee5aa5d8
Paul Jankowski <paul.jankowski@dominos.com>
2019-06-11 00:04:31 -0400

    Bump version

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import model from './model'
 
 const collect = (value: any, prevValue: [any]) => prevValue.concat(value)
 
-program.version('0.0.1')
+program.version('0.0.2')
 
 program
   .command('g <modelName>')
